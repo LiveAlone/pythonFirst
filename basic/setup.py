70ddaa95858e526b21c3da8e0402de9329d9846d
yaoqijun <yaoqijunmail@gmail.com>
2023-10-04 15:29:48 +0800

    add

--- basic/setup.py
+++ basic/setup.py
@@ -5,20 +5,9 @@ __author__ = 'yaoqijun'
 __mail__ = 'yaoqijunmail@foxmail.com'
 
 '''
-description: 
+description: 项目启动类
 '''
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    print("""\
-    Usage: thingy [OPTIONS]
-         -h                        Display this usage message
-         -H hostname               Hostname to connect to
-    """)
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass

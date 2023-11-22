e3ce74c9d1ca64e0ade134346f67fa9cd7e9836a
yaoqijun <yaoqijunmail@gmail.com>
2023-11-22 22:30:01 +0800

    init add first chart

---
+++ fluent/01-data-model/frenchdeck.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yaoqijun'
+__mail__ = 'yaoqijunmail@foxmail.com'
+
+import collections
+
+'''
+description: 卡片
+'''
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, item):
+        return self._cards[item]
+
+
+if __name__ == '__main__':
+    deck = FrenchDeck()
+
+    # 定义排序
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def spades_high(card: Card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    for card in sorted(deck, key=spades_high):
+        print(card)
+
---
+++ fluent/set_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yaoqijun'
+__mail__ = 'yaoqijunmail@foxmail.com'
+
+'''
+description: 
+'''
+
+if __name__ == '__main__':
+    print('hello world')
